c0113dd3183253a5cae6e36917b2c64dd5894473
Antosik <grig_anton96@mail.ru>
2024-05-03 17:05:47 +0300

    feat: move from sveltekit to pure svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -12,16 +11,6 @@ const config = {
 			prependData: `@import '${join(__dirname, 'src/scss/components.scss').replace(/\\/g, '/')}';`
 		}
 	}),
-
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html',
-		}),
-		env: {
-			publicPrefix: '',
-		},
-		prerender: { entries: [] }
-	}
 };
 
 export default config;
