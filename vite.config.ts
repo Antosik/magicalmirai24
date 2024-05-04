f6e059a85513dda1ffa73b9e17ac081aaad1a7fb
Antosik <grig_anton96@mail.ru>
2024-05-04 10:57:09 +0300

    misc: create alias for $lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: resolve(__dirname, './src/lib'),
+    },
+  },
 });
