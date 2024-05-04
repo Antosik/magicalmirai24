0e4aed12d906f22e62bbe9e1fca25bbf352d821a
Antosik <grig_anton96@mail.ru>
2024-05-04 12:05:47 +0300

    fix: fix fonts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,11 @@ const __dirname = dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: resolve(__dirname, './build'),
+  },
   plugins: [svelte()],
+  publicDir: resolve(__dirname, './static'),
   resolve: {
     alias: {
       $lib: resolve(__dirname, './src/lib'),
