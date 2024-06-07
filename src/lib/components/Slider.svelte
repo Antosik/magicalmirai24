856977e826a9bcdda604d2b19ae05c3cf0ab3ca2
Antosik <grig_anton96@mail.ru>
2024-06-07 10:48:20 +0300

    feat: move slider to separate component

---
+++ src/lib/components/Slider.svelte
@@ -0,0 +1,87 @@
+<script lang="ts" generics="T">
+  import { onMount } from 'svelte';
+
+  import FeatherIcon from './FeatherIcon.svelte';
+
+  export let items: T[] = [];
+
+  let slider: HTMLUListElement;
+  let activeElement: HTMLLIElement;
+
+  function toPrevious() {
+    const previous = activeElement.previousElementSibling as HTMLLIElement;
+    if (previous) {
+      activeElement = previous;
+      activeElement.scrollIntoView();
+    }
+  }
+
+  function toNext() {
+    const next = activeElement.nextElementSibling as HTMLLIElement;
+    if (next) {
+      activeElement = next;
+      activeElement.scrollIntoView();
+    }
+  }
+
+  onMount(() => {
+    activeElement = slider.firstChild as HTMLLIElement;
+  });
+</script>
+
+<div>
+  <button type="button" on:click={toPrevious}>
+    <FeatherIcon name="chevron-left" size="24" />
+  </button>
+  <ul bind:this={slider}>
+    {#each items as item, index}
+      <li>
+        <slot {item} {index} />
+      </li>
+    {/each}
+  </ul>
+  <button type="button" on:click={toNext}>
+    <FeatherIcon name="chevron-right" size="24" />
+  </button>
+</div>
+
+<style lang="scss">
+  div {
+    @include flex_vcenter;
+
+    width: 100%;
+    flex: 1;
+  }
+
+  button {
+    @include flex_center;
+
+    padding: grid(1);
+    border: none;
+    background: none;
+  }
+
+  ul {
+    @include flex_vcenter;
+
+    padding: grid(4) 0;
+    gap: grid(5);
+    overflow-x: auto;
+    overscroll-behavior-x: contain;
+    scroll-snap-type: x mandatory;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  li {
+    @include flex_center;
+
+    width: 100%;
+    flex-grow: 1;
+    flex-shrink: 0;
+    scroll-snap-align: center;
+    scroll-snap-stop: always;
+  }
+</style>
--- src/lib/widgets/Help.svelte
+++ src/lib/widgets/Help.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-
-  import FeatherIcon from '$lib/components/FeatherIcon.svelte';
+  import Slider from '$lib/components/Slider.svelte';
   import { getLocale } from '$lib/contexts/locale';
   import { Page, getPage } from '$lib/contexts/page';
 
   const page = getPage();
   const { i18n } = getLocale();
 
-  let slider: HTMLUListElement;
-  let activeElement: HTMLLIElement;
+  const helpItems = [
+    {
+      image: 'miku',
+      text: 'Navigate Miku-san with mouse move or keys',
+    },
+    {
+      image: 'miku',
+      text: 'Navigate Miku-san with mouse move or keys',
+    },
+    {
+      image: 'miku',
+      text: 'Navigate Miku-san with mouse move or keys',
+    },
+    {
+      image: 'miku',
+      text: 'Navigate Miku-san with mouse move or keys',
+    },
+    {
+      image: 'miku',
+      text: 'Navigate Miku-san with mouse move or keys',
+    },
+  ];
 
   function openMainMenu() {
     $page = Page.MAIN_PAGE;
@@ -20,26 +38,6 @@
       openMainMenu();
     }
   }
-
-  function toPrevious() {
-    const previous = activeElement.previousElementSibling as HTMLLIElement;
-    if (previous) {
-      activeElement = previous;
-      activeElement.scrollIntoView();
-    }
-  }
-
-  function toNext() {
-    const next = activeElement.nextElementSibling as HTMLLIElement;
-    if (next) {
-      activeElement = next;
-      activeElement.scrollIntoView();
-    }
-  }
-
-  onMount(() => {
-    activeElement = slider.firstChild as HTMLLIElement;
-  });
 </script>
 
 <svelte:document on:keydown={handleKeyDown} />
@@ -52,34 +50,12 @@
   <h1>{$i18n('Help')}</h1>
 
   <div class="content">
-    <button type="button" on:click={toPrevious}>
-      <FeatherIcon name="chevron-left" size="24" />
-    </button>
-    <ul bind:this={slider}>
-      <li>
+    <Slider items={helpItems}>
+      <article>
         <img src="../images/miku.png" alt="" />
         <p>Navigate Miku-san with mouse move or keys</p>
-      </li>
-      <li>
-        <img src="../images/miku.png" alt="" />
-        <p>Navigate Miku-san with mouse move or keys</p>
-      </li>
-      <li>
-        <img src="../images/miku.png" alt="" />
-        <p>Navigate Miku-san with mouse move or keys</p>
-      </li>
-      <li>
-        <img src="../images/miku.png" alt="" />
-        <p>Navigate Miku-san with mouse move or keys</p>
-      </li>
-      <li>
-        <img src="../images/miku.png" alt="" />
-        <p>Navigate Miku-san with mouse move or keys</p>
-      </li>
-    </ul>
-    <button type="button" on:click={toNext}>
-      <FeatherIcon name="chevron-right" size="24" />
-    </button>
+      </article>
+    </Slider>
   </div>
 </section>
 
@@ -142,21 +118,7 @@
     padding: grid(2) grid(4);
   }
 
-  ul {
-    @include flex_vcenter;
-
-    padding: grid(4) 0;
-    gap: grid(5);
-    overflow-x: auto;
-    overscroll-behavior-x: contain;
-    scroll-snap-type: x mandatory;
-
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  }
-
-  li {
+  artile {
     @include flex_center;
 
     width: 100%;
@@ -164,8 +126,6 @@
     flex-grow: 1;
     flex-shrink: 0;
     gap: grid(2);
-    scroll-snap-align: center;
-    scroll-snap-stop: always;
   }
 
   img {
