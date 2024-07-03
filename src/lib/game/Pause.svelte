9eb68818667d6f1e847e2a2485d65a01086fe79a
Valentina Klepikova <evverything@yandex.ru>
2024-07-04 00:06:50 +0200

    refactor(design): change pause buttons size and relocate it a bit

--- src/lib/game/Pause.svelte
+++ src/lib/game/Pause.svelte
@@ -47,9 +47,9 @@
     max-width: calc(100dvw + grid(4));
     max-height: calc(100dvh + grid(4));
     flex-direction: column;
-    padding: grid(10);
+    padding: grid(12.5) grid(10) grid(10) grid(10);
     aspect-ratio: 1;
-    gap: grid(5);
+    gap: grid(4);
 
     @include breakpoint(xxl) {
       padding: grid(16);
@@ -74,7 +74,7 @@
     transition: transform 200ms ease-in-out;
 
     &:hover {
-      transform: scale(1.3);
+      transform: scale(1.2);
     }
 
     @include breakpoint(md) {
